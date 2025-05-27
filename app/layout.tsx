612feb5e739665eff754063263a1c87e6d839a38
Javier Laris <flopezlaris@gmail.com>
2025-05-27 14:34:35 -0700

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,14 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Finanzas Del Yaqui",
+  title: "PMY App",
   description: "Sistema de gestión financiera para Paquetería & Mensajería Del Yaqui",
-    generator: 'v0.dev'
+  icons: {
+    icon: "/logo-no-fondo.png",
+    apple: "/logo-no-fondo.png",
+    shortcut: "/favicon-32x32.png",
+  },
+  authors: [{ name: "Paquetería del Yaqui © 2025" }],
 }
 
 export default function RootLayout({
