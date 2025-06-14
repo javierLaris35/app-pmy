ed7a477a6fb559c799ae68e3c578c68899c8cb7e
Javier Laris <javier.rappaz@gmail.com>
2025-06-13 20:18:12 -0700

    fix server errors

--- app/operaciones/choferes/page.tsx
+++ app/operaciones/choferes/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 
 import { DataTable } from "@/components/data-table/data-table"
 import { columns } from "./columns"
--- app/operaciones/rutas/page.tsx
+++ app/operaciones/rutas/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { DataTable } from "@/components/data-table/data-table"
 import { columns } from "./columns"
 import { AppLayout } from "@/components/app-layout"
