faee86b6dc12ff57f19b53450a2435bcbff7118f
Javier Laris <flopezlaris@gmail.com>
2025-09-04 19:54:23 -0700

    add electron, fix cors

--- lib/axios-config.ts
+++ lib/axios-config.ts
@@ -4,3 +4,5 @@ export const axiosConfig = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   //withCredentials: true, // si usas cookies
 });
+
+axiosConfig.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
