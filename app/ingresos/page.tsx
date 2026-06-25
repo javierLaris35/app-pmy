f4842022fcc5b70a9f9dc1876ee050012f1a127b
Javier Laris <flopezlaris@gmail.com>
2026-06-24 21:27:58 -0700

    fix incomes warn

--- app/ingresos/page.tsx
+++ app/ingresos/page.tsx
@@ -206,7 +206,7 @@ function IngresosPage() {
           </CardHeader>
           <CardContent className="pt-6 h-[350px]">
             {data?.chartData && data.chartData.length > 0 ? (
-              <ResponsiveContainer width="100%" height="100%">
+              <ResponsiveContainer width="100%" height="100%" initialDimension={{ width: 600, height: 326 }}>
                 <AreaChart data={data.chartData} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
                   <defs>
                     <linearGradient id="colorActual" x1="0" y1="0" x2="0" y2="1">
